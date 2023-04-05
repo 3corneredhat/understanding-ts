5299a3e1fc18dea84007136a3daffc358e1aaa71
3corneredhat <alancabrera10@gmail.com>
2023-04-05 10:20:45 -0500

    feat: finished tuple lecture

--- app.ts
+++ app.ts
@@ -8,12 +8,22 @@
 //   age: 30,
 // };
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple
+} = {
   name: "Alan",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"], //tuple
+  role: [2, "author"],
 };
+
+// person.role.push("admin"); <- no compilation error
+// person.role = [0, 'admin', 'user']; <- but this doesn't work
+
+// person.role[1] = 10; <- compilation error
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports", "Hi"];
