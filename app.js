f93cd0e19aefe10af39ee6bd0eaec0780573c76d
3corneredhat <alancabrera10@gmail.com>
2023-04-05 16:23:00 -0500

    feat: explored ts functions and return types

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-// Unions allow for parameters to be of different types
-// Use the '|' symbol between every possible type.
-// Ex. function func(input1: number | input2: string| input3: boolean)
-// function combine(input1: number | string, input2: number | string) {
-//   let result;
-//   if (typeof input1 === "number" && typeof input2 === "number") {
-//     result = input1 + input2;
-//   } else {
-//     result = input1.toString() + input2.toString();
-//   }
-//   return result;
-// }
-function combine(input1, input2, 
-// The following indicates 'literals' where
-// the specific core type is string but is
-// specifically one of two specific strings:
-// "as-number" | "as-text"
-resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        // '+' in front of the variable converts it to a number.
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 25, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "32", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("max", "anna", "as-text");
-console.log(combinedNames);
+// Return types can be explicitly declared
+// using colon and followed by the type. But
+// it takes away from the power of inference.
+// function add(n1: number, n2: number): string {
+//   return n1.toString() + n2.toString();
+// }
+// The return type in this function is void.
+// It can be declared explicitly.
+function printResult(num) {
+    console.log("Result: " + num);
+}
+// The return type here is undefined, and will
+// rarely be needed.
+//
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,46 +1,26 @@
-// Unions allow for parameters to be of different types
-// Use the '|' symbol between every possible type.
-
-// Ex. function func(input1: number | input2: string| input3: boolean)
-// function combine(input1: number | string, input2: number | string) {
-//   let result;
-//   if (typeof input1 === "number" && typeof input2 === "number") {
-//     result = input1 + input2;
-//   } else {
-//     result = input1.toString() + input2.toString();
-//   }
-
-//   return result;
-// }
-
-// Type Aliases/Custom Types can be used as a short hand for Union types
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    // '+' in front of the variable converts it to a number.
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 25, "as-number");
-console.log(combinedAges);
+// Return types can be explicitly declared
+// using colon and followed by the type. But
+// it takes away from the power of inference.
+// function add(n1: number, n2: number): string {
+//   return n1.toString() + n2.toString();
+// }
 
-const combinedStringAges = combine("30", "32", "as-number");
-console.log(combinedStringAges);
+// The return type in this function is void.
+// It can be declared explicitly.
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
 
-const combinedNames = combine("max", "anna", "as-text");
-console.log(combinedNames);
+// The return type here is undefined, and will
+// rarely be needed.
+//
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
+
+printResult(add(5, 12));
