b2bfea77ba69a1c4a85555e286d79458cfc8c969
3corneredhat <alancabrera10@gmail.com>
2023-04-05 14:24:20 -0500

    feat explored ts literal

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 // Unions allow for parameters to be of different types
 // Use the '|' symbol between every possible type.
-//
-function combine(input1, input2) {
+// Ex. function func(input1: number | input2: string| input3: boolean)
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === "number" && typeof input2 === "number") {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result;
+// }
+function combine(input1, input2, 
+// The following indicates 'literals' where
+// the specific core type is string but is
+// specifically one of two specific strings:
+// "as-number" | "as-text"
+resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        // '+' in front of the variable converts it to a number.
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 25);
+var combinedAges = combine(30, 25, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("max", "anna");
+var combinedStringAges = combine("30", "32", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("max", "anna", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -2,10 +2,33 @@
 // Use the '|' symbol between every possible type.
 
 // Ex. function func(input1: number | input2: string| input3: boolean)
-function combine(input1: number | string, input2: number | string) {
+// function combine(input1: number | string, input2: number | string) {
+//   let result;
+//   if (typeof input1 === "number" && typeof input2 === "number") {
+//     result = input1 + input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+
+//   return result;
+// }
+
+function combine(
+  input1: number | string,
+  input2: number | string,
+  // The following indicates 'literals' where
+  // the specific core type is string but is
+  // specifically one of two specific strings:
+  // "as-number" | "as-text"
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    // '+' in front of the variable converts it to a number.
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
@@ -13,8 +36,11 @@ function combine(input1: number | string, input2: number | string) {
   return result;
 }
 
-const combinedAges = combine(30, 25);
+const combinedAges = combine(30, 25, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("max", "anna");
+const combinedStringAges = combine("30", "32", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("max", "anna", "as-text");
 console.log(combinedNames);
