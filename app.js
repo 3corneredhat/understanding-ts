a301aaf3dc28ffc907024afcd70ea234ed45b456
3corneredhat <alancabrera10@gmail.com>
2023-04-05 10:50:34 -0500

    feat: explored ts enum

--- app.js
+++ app.js
@@ -7,11 +7,45 @@
 //   name: "Max",
 //   age: 30,
 // };
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuple
+// } = {
+//   name: "Alan",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+// enum Role {
+//   ADMIN,
+//   READ_ONLY,
+//   AUTHOR,
+// }
+// When using enums, the default is to use
+// 0 as the starting number. We can use
+// any value we want.
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+    Role[Role["USER"] = 201] = "USER";
+})(Role || (Role = {}));
+console.log(Role);
 var person = {
     name: "Alan",
     age: 30,
     hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN,
 };
+// person.role.push("admin"); <- no compilation error
+// person.role = [0, 'admin', 'user']; <- but this doesn't work
+// person.role[1] = 10; <- compilation error
 var favoriteActivities;
 favoriteActivities = ["Sports", "Hi"];
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -8,21 +8,48 @@
 //   age: 30,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //tuple
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuple
+// } = {
+//   name: "Alan",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// enum Role {
+//   ADMIN,
+//   READ_ONLY,
+//   AUTHOR,
+// }
+
+// When using enums, the default is to use
+// 0 as the starting number. We can use
+// any value we want.
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+  USER, //is set to 201
+}
+
+console.log(Role);
+const person = {
   name: "Alan",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin"); <- no compilation error
 // person.role = [0, 'admin', 'user']; <- but this doesn't work
-
 // person.role[1] = 10; <- compilation error
 
 let favoriteActivities: string[];
