b1b6297aa768cfffe57f7456501e0e543f6b86a9
3corneredhat <alancabrera10@gmail.com>
2023-04-11 14:23:25 -0500

    feat: explored unknown type

---
+++ functions.ts
@@ -0,0 +1,52 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// Return types can be explicitly declared
+// using colon and followed by the type. But
+// it takes away from the power of inference.
+// function add(n1: number, n2: number): string {
+//   return n1.toString() + n2.toString();
+// }
+
+// The return type in this function is void.
+// It can be declared explicitly.
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+// The return type here is undefined, and will
+// rarely be needed.
+//
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
+
+// Callback functions are similar to js only
+// now parameters are enforced.
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
+
+// A variable can also be declared as a
+// Function type.
+//
+// let combineValues: () => number;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+// The following assignment gives us a compiler error
+// because the assigned function needs to meet the criteria
+// above:
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
